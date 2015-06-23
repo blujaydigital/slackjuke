1f881418752654c5e607204f767132729e6f5899
Dave Potts <dave@roostercreativemarketing.com>
2015-06-23 14:04:42 +0100

    htaccess changes

--- index.php
+++ index.php
@@ -33,6 +33,6 @@ switch ($action){
         $spotify->add();
         break;
     default:
-        echo '<a href="/auth">Log In</a>';
+        echo '<a href="/slackbot/spotifyjuke/auth">Log In</a>';
         break;
 }
