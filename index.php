15f81abdc9d83ad68f475112a3ffb70ae6fd654d
Dave Potts <dave@roostercreativemarketing.com>
2015-06-23 16:03:57 +0100

    htaccess changes

--- index.php
+++ index.php
@@ -1,27 +1,26 @@
 <?php
 session_start();
-include ('lib/SpotifyJuke.php');
+include('lib/SpotifyJuke.php');
 
 require('vendor/autoload.php');
 
-use SpotifyWebAPI\Session   as Session;
-use Lib\SpotifyJuke         as SpotifyJuke;
+use Lib\SpotifyJuke as SpotifyJuke;
+use SpotifyWebAPI\Session as Session;
 
 $dotenv = new Dotenv\Dotenv(__DIR__);
 $dotenv->load();
 
 $provider = new Session(
-                    getenv('SPOTIFY_KEY'),
-                    getenv('SPOTIFY_SECRET'),
-                    getenv('SPOTIFY_REDIRECT')
-                );
+    getenv('SPOTIFY_KEY'),
+    getenv('SPOTIFY_SECRET'),
+    getenv('SPOTIFY_REDIRECT')
+);
 
 $spotify = new SpotifyJuke($provider);
 
-
 $action = isset($_GET['action']) ? trim($_GET['action'], '/') : '';
 
-switch ($action){
+switch ($action) {
     case 'auth':
         $spotify->auth();
         break;
--- lib/SpotifyJuke.php
+++ lib/SpotifyJuke.php
@@ -1,33 +1,47 @@
 <?php
 namespace Lib;
 
+use SpotifyWebAPI\Session as Session;
 use SpotifyWebAPI\SpotifyWebAPI as SpotifyWebAPI;
 
-class SpotifyJuke {
+class SpotifyJuke
+{
 
     private $_provider;
     private $_refreshToken;
     private $_db;
 
-    public function __construct (\SpotifyWebAPI\Session $provider){
+    /**
+     * @param Session $provider
+     */
+    public function __construct(Session $provider)
+    {
         $this->_provider = $provider;
         $this->_db = new \SQLite3("db/test.db");
     }
 
-    public function auth (){
-        // If we don't have an authorization code then get one
+    /**
+     * Auth function, redirect the user to
+     * the oauth login page
+     */
+    public function auth()
+    {
         $scopes = ['playlist-modify-public', 'playlist-modify-private'];
 
         $authUrl = $this->_provider->getAuthorizeUrl(array(
             'scope' => $scopes
         ));
 
-        header('Location: '.$authUrl);
+        header('Location: ' . $authUrl);
         die();
     }
 
-    public function callback(){
-
+    /**
+     * Handle the response from Spotify
+     * and store the Access tokens.
+     */
+    public function callback()
+    {
         $this->_provider->requestAccessToken($_GET['code']);
         $this->_refresh();
         // See if an Sqlite DB exists
@@ -35,59 +49,81 @@ class SpotifyJuke {
         $this->_getRefreshToken();
     }
 
-    public function add(){
+    /**
+     * Update the Access/ refresh tokens
+     */
+    private function _refresh()
+    {
+        $this->_provider->refreshAccessToken();
+        $this->_db->query('UPDATE access SET token = "' . $this->_provider->getAccessToken() . '"  WHERE key = "refresh"');
+        $this->_db->query('UPDATE access SET token = "' . $this->_provider->getRefreshToken() . '" WHERE key = "refresh"');
+    }
 
+    /**
+     * Create the SQLite database if it
+     * doesn't already exist.
+     */
+    private function _createDB()
+    {
+        $result = $this->_db->query("SELECT name FROM sqlite_master WHERE type='table' AND name='access'");
+        if (!$result) { // Table doesn't exist, create it!
+            $this->_db->exec('CREATE TABLE access (key STRING, token STRING)');
+            $this->_db->exec("INSERT INTO access (key, token) VALUES ('access', '" . $this->_provider->getAccessToken() . "')");
+            $this->_db->exec("INSERT INTO access (key, token) VALUES ('refresh', '" . $this->_provider->getRefreshToken() . "')");
+        }
+    }
+
+    /**
+     * Returns the refresh token
+     */
+    private function _getRefreshToken()
+    {
+        $result = $this->_db->query('SELECT key, token FROM access WHERE key = "refresh"');
+        $result = $result->fetchArray();
+
+        return $result['token'];
+    }
+
+    /**
+     * Handle the Slack Command
+     */
+    public function add()
+    {
         $this->_provider->setRefreshToken($this->_getRefreshToken());
         $this->_provider->refreshAccessToken();
 
         $api = new SpotifyWebAPI;
-       
+
         $api->setAccessToken($this->_getToken());
         $search = $_POST['text'];
 
-        if (strlen($search) < 2){
+        if (strlen($search) < 2) {
             echo 'Please enter in a more specific search phrase';
         }
 
         // Find out what we searched for
-        $tracks     = $api->search($search, 'track');
-        $track      = $tracks->tracks->items[0]; // Get the first track
+        $tracks = $api->search($search, 'track');
+        $track = $tracks->tracks->items[0]; // Get the first track
 
-        $track_id   = $track->id;
+        $track_id = $track->id;
         $track_name = $track->name;
 
-        // See if the track already exists 
-        if($api->addUserPlaylistTracks(getenv('SPOTIFY_USERNAME'), getenv('SPOTIFY_PLAYLIST'), $track_id )){
-                echo $track->artists[0]->name. ' - '. $track_name . " added!";
-        }else {
-                echo $track->artists[0]->name. ' - '. $track_name . " failed to add!";
+        // See if the track already exists
+        if ($api->addUserPlaylistTracks(getenv('SPOTIFY_USERNAME'), getenv('SPOTIFY_PLAYLIST'), $track_id)) {
+            echo $track->artists[0]->name . ' - ' . $track_name . " added!";
+        } else {
+            echo $track->artists[0]->name . ' - ' . $track_name . " failed to add!";
         }
     }
 
-    private function _refresh(){
-        $this->_provider->refreshAccessToken();
-        $result = $this->_db->query('UPDATE access SET token = "'.$this->_provider->getAccessToken().'"  WHERE key = "refresh"');
-        $result = $this->_db->query('UPDATE access SET token = "'.$this->_provider->getRefreshToken().'" WHERE key = "refresh"');
-    }
-
-    private function _getToken(){
+    /**
+     * Get the Access token from the DB
+     */
+    private function _getToken()
+    {
         $result = $this->_db->query('SELECT key, token FROM access WHERE key = "access"');
         $result = $result->fetchArray();
-        return $result['token'];
-    }
 
-    private function _getRefreshToken(){
-        $result = $this->_db->query('SELECT key, token FROM access WHERE key = "refresh"');
-        $result = $result->fetchArray();
         return $result['token'];
-    }
-
-    private function _createDB(){
-        $result = $this->_db->query("SELECT name FROM sqlite_master WHERE type='table' AND name='access'");
-        if (!$result){ // Table doesn't exist, create it!
-            $this->_db->exec('CREATE TABLE access (key STRING, token STRING)');
-            $this->_db->exec("INSERT INTO access (key, token) VALUES ('access', '".$this->_provider->getAccessToken()."')");
-            $this->_db->exec("INSERT INTO access (key, token) VALUES ('refresh', '".$this->_provider->getRefreshToken()."')");
-        }
     }
 }
